7dc7cd337c2adddce7eeb6b33c1cb4c6096068d2
Muhammad-Hassan-email <89068710+Muhammad-Hassan-email@users.noreply.github.com>
2024-11-12 16:52:42 +0500

    Add files via upload

---
+++ model/QUestionSummary.dart
@@ -0,0 +1,24 @@
+import 'package:challange_3/model/question_summary/Summary_item.dart';
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return SummaryItem(data);
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
---
+++ model/QuizQuestions.dart
@@ -0,0 +1,12 @@
+class QuizQuestions {
+  const QuizQuestions(this.text, this.answer);
+
+  final String text;
+  final List<String> answer;
+
+  List<String> getShuffledAnswers() {
+    final ShuffledList = List.of(answer);
+    ShuffledList.shuffle();
+    return ShuffledList;
+  }
+}
